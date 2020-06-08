93cd7577c6a20f313f1479d6fcd29d18c6d70b66
nguyentiendien0907@outlook.com <nguyentiendien0907@outlook.com>
2020-06-08 09:28:29 +0700

    Add project files.

---
+++ NTD_lab456/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NTD_lab456.Startup))]
+namespace NTD_lab456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
